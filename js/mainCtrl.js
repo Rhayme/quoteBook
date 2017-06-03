ae0d6dd164c969e4d1672dc6ee81902039b08194
Ray Pena <rhayme00@gmail.com>
2017-06-02 23:42:38 -0600

    Project Completed

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,23 @@
 /**
  * Created by Rayme on 6/2/2017.
  */
-angular.module('quoteBook').controller('mainCtrl', function ($scope) {
-    $scope.test = "hello";
-})
+angular.module('quoteBook').controller('mainCtrl', function ($scope, dataService) {
+   $scope.quotes = dataService.getQuotes();
+
+   //remove data from the list if the delete button is click
+  $scope.deleteMe = function (textToDelete) {
+        dataService.removeData(textToDelete);
+  };
+
+  $scope.addQuote = function () {
+     var newQuote = {
+         text: $scope.newQuoteText,
+         author: $scope.newQuoteAuthor
+     }
+     if(dataService.addData(newQuote)){
+         $scope.newQuoteText = "";
+         $scope.newQuoteAuthor = "";
+     }
+  }
+
+});
